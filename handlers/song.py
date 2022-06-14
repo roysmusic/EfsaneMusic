ed3ad8cab102eeaf94e9d0a1c30c789255db9fef
emirpng <marveldarkbull@gmail.com>
2022-06-14 13:26:25 +0300

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -55,7 +55,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☑️ **İsmi**: [{title[:35]}]({link})\n📤 **Tarafından**: @RolRoys"
+        rep = f"☑️ **İsmi**: [{title[:35]}]({link})\nDaha fazlası için ☞ @BirazdaMuzik"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
